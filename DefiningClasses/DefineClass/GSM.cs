5183b10fa231e0504ad40ee726818e7f777fd7c7
Georgi Markov <markovood85@gmail.com>
2016-07-16 18:19:26 +0300

    started task 1

---
+++ DefiningClasses/DefineClass/GSM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefineClass
+{
+    // Define a class that holds information about a mobile phone device: model, manufacturer, price, owner, battery characteristics
+    // (model, hours idle and hours talk) and display characteristics (size and number of colors). Define 3 separate classes (class GSM
+    // holding instances of the classes Battery and Display).
+    class GSM
+    {
+
+        static void Main()
+        {
+        }
+    }
+}
