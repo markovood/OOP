02f4ac91d30e1106740128e3b4fe3aa3a3998c2a
Georgi Markov <markovood85@gmail.com>
2016-07-31 01:32:24 +0300

    solved tasks 1 to 6 inclusive

---
+++ DefiningClasses-Part2/GenericClass/GenericList.cs
@@ -0,0 +1,144 @@
+﻿namespace GenericClass
+{
+    using System;
+    using System.Text;
+
+    public class GenericList<T>
+    {
+        // fields
+        private T[] list;
+
+        private int index;
+        private int count;
+
+        // constructors
+        public GenericList(int capacity)
+        {
+            this.list = new T[capacity];
+            this.index = 0;
+            this.count = 0;
+        }
+
+        // properties
+        public int Count
+        {
+            get
+            {
+                return this.count;
+            }
+        }
+
+        // indexer
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                return this.list[index];
+            }
+
+            set
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                this.list[index] = value;
+            }
+        }
+
+        // methods
+        public void Add(T item)
+        {
+            // ensure capacity
+            if (this.index == this.list.Length)
+            {
+                // enlarge the array double
+                this.EnsureCapacity();
+            }
+
+            this.list[this.index] = item;
+            this.index++;
+            this.count++;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index > this.count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            // ensure capacity
+            if (this.index == this.list.Length)
+            {
+                // enlarge the array double
+                this.EnsureCapacity();
+            }
+
+            Array.Copy(this.list, index, this.list, index + 1, this.count - index);
+            this.list[index] = item;
+            this.index++;
+            this.count++;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            this.count--;
+            Array.Copy(this.list, index + 1, this.list, index, this.count - index);
+            this.list[this.count] = default(T);
+            this.index--;
+        }
+
+        public void Clear()
+        {
+            if (this.count > 0)
+            {
+                Array.Clear(this.list, 0, this.count);
+                this.index = 0;
+                this.count = 0;
+            }
+        }
+
+        public int IndexOf(T element)
+        {
+            return Array.IndexOf(this.list, element);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder listAsStr = new StringBuilder();
+            for (int i = 0; i < this.count; i++)
+            {
+                if (i == this.count - 1)
+                {
+                    listAsStr.Append(this.list[i].ToString());
+                }
+                else
+                {
+                    listAsStr.Append(this.list[i].ToString() + ",\r\n");
+                }
+            }
+
+            return listAsStr.ToString();
+        }
+
+        private void EnsureCapacity()
+        {
+            T[] enlargedArr = new T[this.list.Length * 2];
+            Array.Copy(this.list, 0, enlargedArr, 0, this.count);
+            this.list = enlargedArr;
+            enlargedArr = null;
+        }
+    }
+}
--- DefiningClasses-Part2/Test/TestingPrgm.cs
+++ DefiningClasses-Part2/Test/TestingPrgm.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EuclidianPoint;
+using GenericClass;
 
 namespace Test
 {
@@ -11,6 +12,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            // tasks 1-4
             var point = new Point3D(1, 3, 5);
             var point2 = new Point3D(2, 6, 1);
 
@@ -34,7 +36,51 @@ namespace Test
             pointsCollection.AddPoint(point2);
 
             PathStorage.SavePath(pointsCollection, @"../../result.txt");
-            var somePath = PathStorage.LoadPath(@"../../pointsList.txt"); ;
+            var somePath = PathStorage.LoadPath(@"../../pointsList.txt");
+
+            // tasks 5-7
+            var list = new GenericList<Point3D>(16);
+            list.Add(new Point3D(3, 2, 1));
+            list.Add(new Point3D(6, 2, 0));
+            list.Add(new Point3D(9, 4, 1));
+            list.Add(new Point3D(12, 2, 11));
+            list.Add(new Point3D(11, 1, 90));
+            list.Add(new Point3D(8, 3, 100));
+            // testing validations uncomment to see result
+            // var element = list[-1];
+            // var element = list[9];
+            // list[-1] = Point3D.PointO;
+            // list[9] = Point3D.PointO;
+            var elementToRemove = list[2];
+            int listCount = list.Count;
+            // testing validations uncomment to see result
+            // list.RemoveAt(-1);
+            // list.RemoveAt(9);
+            list.RemoveAt(2);
+            listCount = list.Count;
+            list.Add(elementToRemove);
+            listCount = list.Count;
+            var elementToInsert = new Point3D(3, 3, 3);
+            list.Insert(2, elementToInsert);
+            // testing validations uncomment to see result
+            // list.Insert(8, elementToInsert);
+            list.Insert(7, elementToInsert);
+            // testing Clear() uncomment to see result
+            // list.Clear();
+            int elementToInsIndex = list.IndexOf(elementToInsert);  // will return -1 always cause we've cleared the list the previous step
+            Console.WriteLine(list);
+
+            list.Add(new Point3D(3, 2, 1));
+            list.Add(new Point3D(1, 1, 1));
+            list.Add(new Point3D(2, 2, 2));
+            list.Add(new Point3D(33, 33, 33));
+            list.Add(new Point3D(44, 55, 66));
+            list.Add(new Point3D(100, 101, 102));
+            list.Add(new Point3D(22, 22, 22));
+            list.Add(new Point3D(77, 33, 77));
+            // testing EnsureCapacity() in .Add() and in .Insert()
+            list.Add(new Point3D(99, 55, 88));
+            // list.Insert(16, new Point3D(99, 55, 88));
 
         }
     }
