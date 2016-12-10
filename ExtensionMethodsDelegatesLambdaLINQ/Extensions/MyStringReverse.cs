19cc79cfd185b83d106d0203d0e9b7c209abf107
Georgi Markov <markovood85@gmail.com>
2016-12-10 22:39:11 +0200

    added new extenssion method

---
+++ ExtensionMethodsDelegatesLambdaLINQ/Extensions/MyStringReverse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExtensions
+{
+    public static class MyStringReverse
+    {
+        public static string MyReverse(this string someString)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = someString.Length - 1; i >= 0; i--)
+            {
+                result.Append(someString[i]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
