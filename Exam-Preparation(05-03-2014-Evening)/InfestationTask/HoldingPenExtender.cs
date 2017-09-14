8da269f8101c46b1a845bf3fd5b3a42e2dd72e91
Georgi Markov <markov_ood@abv.bg>
2017-09-14 20:24:24 +0300

    -Implemented commands for inserting all units -Implemented commands for adding supplements

--- Exam-Preparation(05-03-2014-Evening)/InfestationTask/HoldingPenExtender.cs
+++ Exam-Preparation(05-03-2014-Evening)/InfestationTask/HoldingPenExtender.cs
@@ -7,9 +7,56 @@ namespace Infestation
 {
     public class HoldingPenExtender : HoldingPen
     {
+        protected override void ExecuteInsertUnitCommand(string[] commandWords)
+        {
+            switch (commandWords[1])
+            {
+                case "Marine":
+                    var marine = new Marine(commandWords[2]);
+                    this.InsertUnit(marine);
+                    break;
+                case "Tank":
+                    var tank = new Tank(commandWords[2]);
+                    this.InsertUnit(tank);
+                    break;
+                case "Queen":
+                    var queen = new Queen(commandWords[2]);
+                    this.InsertUnit(queen);
+                    break;
+                case "Parasite":
+                    var parasite = new Parasite(commandWords[2]);
+                    this.InsertUnit(parasite);
+                    break;
+                default:
+                    base.ExecuteInsertUnitCommand(commandWords);
+                    break;
+            }
+        }
+
         protected override void ExecuteAddSupplementCommand(string[] commandWords)
         {
-            
+            ISupplement supplement;
+            switch (commandWords[1])
+            {
+                case "PowerCatalyst":
+                    supplement = new PowerCatalyst();
+                    this.GetUnit(commandWords[2]).AddSupplement(supplement);
+                    break;
+                case "HealthCatalyst":
+                    supplement = new HealthCatalyst();
+                    this.GetUnit(commandWords[2]).AddSupplement(supplement);
+                    break;
+                case "AggressionCatalyst":
+                    supplement = new AggressionCatalyst();
+                    this.GetUnit(commandWords[2]).AddSupplement(supplement);
+                    break;
+                case "Weapon":
+                    supplement = new Weapon();
+                    this.GetUnit(commandWords[2]).AddSupplement(supplement);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
