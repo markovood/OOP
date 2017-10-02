018c12389f5106779b9333b53c05f446854462b9
Georgi Markov <markov_ood@abv.bg>
2017-10-02 14:32:37 +0300

    Completed HTMLRenderer task

---
+++ ExapPreparation(25-03-2013-Evening)/HTMLRenderer/Element.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLRenderer
+{
+    public class Element : IElement
+    {
+        private List<IElement> childElements;
+
+        public Element(string name)
+        {
+            this.Name = name;
+            this.childElements = new List<IElement>();
+        }
+
+        public Element(string name, string content)
+        {
+            this.Name = name;
+            this.TextContent = content;
+            this.childElements = new List<IElement>();
+        }
+
+        public virtual IEnumerable<IElement> ChildElements
+        {
+            get
+            {
+                return this.childElements;
+            }
+        }
+
+        public string Name { get; }
+
+        public string TextContent { get; set; }
+
+        public void AddElement(IElement element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException("element");
+            }
+
+            this.childElements.Add(element);
+        }
+
+        public virtual void Render(StringBuilder output)
+        {
+            if (this.Name != null)
+            {
+                output.AppendFormat("<{0}>", this.Name);
+
+                output.AppendFormat(
+                    "{0}{1}",
+                    this.TextContent == null ? this.TextContent : EncodeHTML(this.TextContent),
+                    string.Join("", this.ChildElements));
+
+                output.AppendFormat("</{0}>", this.Name);
+            }
+            else
+            {
+                output.AppendFormat(
+                    "{0}{1}",
+                    this.TextContent == null ? this.TextContent : EncodeHTML(this.TextContent),
+                    string.Join("", this.ChildElements));
+            }
+
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            this.Render(output);
+
+            return output.ToString();
+        }
+
+        private string EncodeHTML(string textContent)
+        {
+            return textContent.
+                Replace("&", "&amp;").
+                Replace("<", "&lt;").
+                Replace(">", "&gt;");
+        }
+    }
+}
---
+++ ExapPreparation(25-03-2013-Evening)/HTMLRenderer/Table.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLRenderer
+{
+    public class Table : Element, ITable
+    {
+        private IElement[,] tableMatrix;
+
+        public Table(int rows, int cols) : 
+            base("table")
+        {
+            this.TextContent = null;
+            this.ChildElements = null;
+            this.Rows = rows;
+            this.Cols = cols;
+            this.tableMatrix = new IElement[this.Rows, this.Cols];
+        }
+
+        public IElement this[int row, int col]
+        {
+            get
+            {
+                return this.tableMatrix[row, col];
+            }
+
+            set
+            {
+                this.tableMatrix[row, col] = value;
+            }
+        }
+
+        public int Cols { get; }
+
+        public int Rows { get; }
+
+        public new IEnumerable<IElement> ChildElements { get; private set; }
+
+        public override void Render(StringBuilder output)
+        {
+            output.AppendFormat("<{0}>",this.Name);
+
+            for (int row = 0; row < this.tableMatrix.GetLength(0); row++)
+            {
+                output.Append("<tr>");
+                for (int col = 0; col < this.tableMatrix.GetLength(1); col++)
+                {
+                    output.AppendFormat("<td>{0}</td>", this.tableMatrix[row, col]);
+                }
+
+                output.Append("</tr>");
+            }
+
+            output.AppendFormat("</{0}>", this.Name);
+        }
+    }
+}
